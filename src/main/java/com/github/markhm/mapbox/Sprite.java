0e99ba37f54df214c22e8e79bfe0d22ba54eb287
Mark <github@hissinkmuller.nl>
2020-01-28 21:00:26 +0100

    Mapbox Maki Sprites, which are not all available

---
+++ src/main/java/com/github/markhm/mapbox/Sprite.java
@@ -0,0 +1,55 @@
+package com.github.markhm.mapbox;
+
+/* A selection of Mapbox Maki Sprites. Full list via: https://labs.mapbox.com/maki-icons/.
+ */
+public class Sprite
+{
+    public static final String Airport = "airport";
+    public static final String Harbor = "harbor";
+    public static final String Helicopter = "heliport";
+
+    public static final String Airplane = "airport";
+    public static final String Bicycle = "bicycle";
+    public static final String Boat = "ferry";
+    public static final String Bus = "bus";
+    public static final String Car = "car";
+    public static final String Rocket = "rocket";
+    public static final String Train = "rail";
+
+    public static final String Doctor = "doctor";
+    public static final String Police = "police";
+    public static final String Fire_Station = "fire-station";
+    public static final String Hospital = "hospital";
+
+    public static final String Danger = "danger";
+    public static final String Globe = "globe";
+    public static final String Information = "information";
+    public static final String Marker = "marker";
+    public static final String Marker_Open = "marker-stroked";
+
+    public static final String Flag = "embassy";
+    public static final String Cross = "cross";
+    public static final String Heart = "heart";
+    public static final String Star = "star";
+    public static final String Star_Open = "star-stroked";
+    public static final String Circle = "circle";
+    public static final String Circle_Open = "circle-stroked";
+    public static final String Triangle = "triangle";
+    public static final String Triangle_Open = "circle-stroked";
+
+    public static final String Home = "home";
+
+    public static final String Monument = "monument";
+    public static final String Bank = "bank";
+    public static final String Building = "building";
+    public static final String Castle = "castle";
+    public static final String Commercial = "commercial";
+    public static final String Convenience = "convenience";
+    public static final String Factory = "industry";
+    public static final String Farm = "farm";
+    public static final String Tulip = "garden";
+    public static final String Shop = "shop";
+    public static final String Town_hall = "town-hall";
+    public static final String Warehouse = "warehouse";
+    public static final String Windmill = "windmil";
+}
