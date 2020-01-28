97517c45ed8f3518f39b5989d97849b185fff75d
Mark <github@hissinkmuller.nl>
2020-01-28 21:06:30 +0100

    Test loading the access token

---
+++ src/test/java/com/github/markhm/mapbox/AccessTokenTest.java
@@ -0,0 +1,22 @@
+package com.github.markhm.mapbox;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.jupiter.api.Test;
+
+public class AccessTokenTest
+{
+    private Log log = LogFactory.getLog(AccessTokenTest.class);
+
+    public AccessTokenTest()
+    {}
+
+    @Test
+    public void testPropertyFileLoad()
+    {
+        String fileLocation = AccessToken.getJSFileLocation();
+        log.warn("fileLocation = "+fileLocation);
+
+    }
+
+}
