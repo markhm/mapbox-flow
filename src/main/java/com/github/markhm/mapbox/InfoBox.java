542cd149f51a85b97b44e473a5e52edc92d46be4
Mark <github@hissinkmuller.nl>
2020-01-28 20:58:55 +0100

    For coordinates

---
+++ src/main/java/com/github/markhm/mapbox/InfoBox.java
@@ -0,0 +1,12 @@
+package com.github.markhm.mapbox;
+
+import com.vaadin.flow.component.html.Div;
+
+public class InfoBox extends Div
+{
+    public InfoBox()
+    {
+        setId("info");
+    }
+
+}
