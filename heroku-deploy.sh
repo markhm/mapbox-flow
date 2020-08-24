d4607c096b9a4add57fec9a772333cabe5fad8c1
Mark <github@hissinkmuller.nl>
2020-08-24 14:24:52 +0200

    Updated deploy script

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -4,5 +4,5 @@ echo
 echo "Deploying to mapbox-flow on Heroku"
 echo
 
-heroku war:deploy target/mapbox-flow-0.0.5-SNAPSHOT.war --app mapbox-flow
+heroku war:deploy target/mapbox-flow-0.0.6-SNAPSHOT.war --app mapbox-flow
 
